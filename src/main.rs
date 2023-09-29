4feebd06d6a0484112787259b9e5541a3f270213
Lőrinc Serfőző <lorinc.serfozo@mailbox.org>
2023-09-29 21:49:38 +0200

    Added 2-pane app Window

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 mod timer;
 
+use std::convert::identity;
+
 use gtk::prelude::{BoxExt, ButtonExt, OrientableExt, WidgetExt};
+use relm4::gtk::gio;
+use relm4::gtk::prelude::GtkWindowExt;
+use relm4::Controller;
 use relm4::{
     adw,
-    gtk::{self},
-    Component, ComponentParts, ComponentSender, RelmApp, RelmWidgetExt, WorkerController,
+    gtk::{self, prelude::ObjectExt},
+    Component, ComponentController, ComponentParts, ComponentSender, RelmApp, RelmObjectExt,
+    RelmWidgetExt, SimpleComponent, WorkerController,
 };
 use timer::{TimerModel, TimerOutput};
 
 #[derive(Debug)]
-struct Exercise {
+struct ExerciseSetup {
     warmup_s: usize,
     exercise_s: usize,
     rest_s: usize,
@@ -23,7 +29,7 @@ enum ExerciseState {
     Rest,
 }
 
-impl Default for Exercise {
+impl Default for ExerciseSetup {
     fn default() -> Self {
         Self {
             warmup_s: 2,
@@ -34,8 +40,8 @@ impl Default for Exercise {
     }
 }
 
-struct AppModel {
-    exercise: Exercise,
+struct ExerciseModel {
+    setup: ExerciseSetup,
     state: ExerciseState,
     remaining_sets: usize,
     remaining_s: usize,
@@ -43,22 +49,22 @@ struct AppModel {
     timer: Option<WorkerController<TimerModel>>,
 }
 
-impl AppModel {
-    fn new(exercise: Exercise, sender: &ComponentSender<AppModel>) -> Self {
+impl ExerciseModel {
+    fn new(exercise: ExerciseSetup, sender: &ComponentSender<ExerciseModel>) -> Self {
         Self {
             state: ExerciseState::Warmup,
             remaining_sets: exercise.sets,
             remaining_s: exercise.warmup_s,
             running: true,
             timer: build_timer(sender),
-            exercise,
+            setup: exercise,
         }
     }
 
-    fn reset(&mut self, sender: &ComponentSender<AppModel>) {
+    fn reset(&mut self, sender: &ComponentSender<ExerciseModel>) {
         self.state = ExerciseState::Warmup;
-        self.remaining_sets = self.exercise.sets;
-        self.remaining_s = self.exercise.warmup_s;
+        self.remaining_sets = self.setup.sets;
+        self.remaining_s = self.setup.warmup_s;
         self.running = true;
         self.timer = build_timer(sender);
     }
@@ -71,7 +77,7 @@ enum AppInput {
     Reset,
 }
 
-fn build_timer(sender: &ComponentSender<AppModel>) -> Option<WorkerController<TimerModel>> {
+fn build_timer(sender: &ComponentSender<ExerciseModel>) -> Option<WorkerController<TimerModel>> {
     Some(
         TimerModel::builder()
             .detach_worker(())
@@ -90,75 +96,68 @@ fn remaining_str(remaining_s: usize) -> String {
 }
 
 #[relm4::component]
-impl Component for AppModel {
-    type Init = usize;
+impl Component for ExerciseModel {
+    type Init = ();
     type Input = AppInput;
     type Output = ();
     type CommandOutput = ();
 
     view! {
-        adw::Window {
+        adw::Clamp {
+            set_orientation: gtk::Orientation::Horizontal,
             gtk::Box {
-                set_spacing: 5,
                 set_orientation: gtk::Orientation::Vertical,
-                adw::HeaderBar {},
-                adw::Clamp {
-                    set_orientation: gtk::Orientation::Horizontal,
-                    gtk::Box {
-                        set_orientation: gtk::Orientation::Vertical,
-                        set_valign: gtk::Align::Center,
-                        gtk::Box {
-                            set_class_active: ("timer", true),
-                            set_class_active: ("card", true),
-                            #[watch]
-                            set_class_active: ("timer-warmup", model.state == ExerciseState::Warmup),
-                            #[watch]
-                            set_class_active: ("timer-exercise", model.state == ExerciseState::Exercise),
-                            #[watch]
-                            set_class_active: ("timer-rest", model.state == ExerciseState::Rest),
-                            set_spacing: 5,
-                            set_orientation: gtk::Orientation::Vertical,
-                            set_valign: gtk::Align::Center,
-                            set_margin_all: 20,
-                            set_vexpand: true,
-                            gtk::Label {
-                                set_class_active: ("title-2", true),
-                                #[watch]
-                                set_label: match model.state {
-                                    ExerciseState::Warmup => "Warm up",
-                                    ExerciseState::Exercise => "Exercise",
-                                    ExerciseState::Rest => "Rest",
-                                },
-                            },
-                            gtk::Label {
-                                set_class_active: ("title-1", true),
-                                #[watch]
-                                set_label: &remaining_str(model.remaining_s),
-                            },
-                            gtk::Box {
-                                set_orientation: gtk::Orientation::Horizontal,
-                                set_halign: gtk::Align::Center,
-                                set_class_active: ("linked", true),
-                                gtk::Button {
-                                    #[watch]
-                                    set_label: if model.running { "Pause" } else { "Resume" },
-                                    #[watch]
-                                    set_sensitive: model.remaining_s != 0,
-                                    connect_clicked => AppInput::StartStop,
-                                },
-                                gtk::Button {
-                                    set_label: "Restart",
-                                    connect_clicked => AppInput::Reset,
-                                    #[watch]
-                                    set_class_active: ("suggested-action", model.remaining_s == 0),
-                                }
-                            }
-                        },
-                        gtk::Label {
-                            #[watch]
-                            set_label: &format!("Remaining sets: {}", model.remaining_sets),
+                set_valign: gtk::Align::Center,
+                gtk::Box {
+                    set_class_active: ("timer", true),
+                    set_class_active: ("card", true),
+                    #[watch]
+                    set_class_active: ("timer-warmup", model.state == ExerciseState::Warmup),
+                    #[watch]
+                    set_class_active: ("timer-exercise", model.state == ExerciseState::Exercise),
+                    #[watch]
+                    set_class_active: ("timer-rest", model.state == ExerciseState::Rest),
+                    set_spacing: 5,
+                    set_orientation: gtk::Orientation::Vertical,
+                    set_valign: gtk::Align::Center,
+                    set_margin_all: 20,
+                    set_vexpand: true,
+                    gtk::Label {
+                        set_class_active: ("title-2", true),
+                        #[watch]
+                        set_label: match model.state {
+                            ExerciseState::Warmup => "Warm up",
+                            ExerciseState::Exercise => "Exercise",
+                            ExerciseState::Rest => "Rest",
                         },
                     },
+                    gtk::Label {
+                        set_class_active: ("title-1", true),
+                        #[watch]
+                        set_label: &remaining_str(model.remaining_s),
+                    },
+                    gtk::Box {
+                        set_orientation: gtk::Orientation::Horizontal,
+                        set_halign: gtk::Align::Center,
+                        set_class_active: ("linked", true),
+                        gtk::Button {
+                            #[watch]
+                            set_label: if model.running { "Pause" } else { "Resume" },
+                            #[watch]
+                            set_sensitive: model.remaining_s != 0,
+                            connect_clicked => AppInput::StartStop,
+                        },
+                        gtk::Button {
+                            set_label: "Restart",
+                            connect_clicked => AppInput::Reset,
+                            #[watch]
+                            set_class_active: ("suggested-action", model.remaining_s == 0),
+                        }
+                    }
+                },
+                gtk::Label {
+                    #[watch]
+                    set_label: &format!("Remaining sets: {}", model.remaining_sets),
                 },
             },
         }
@@ -190,7 +189,7 @@ impl Component for AppModel {
             }
             ",
         );
-        let model = AppModel::new(Exercise::default(), &sender);
+        let model = ExerciseModel::new(ExerciseSetup::default(), &sender);
         let widgets = view_output!();
         ComponentParts { model, widgets }
     }
@@ -217,7 +216,7 @@ impl Component for AppModel {
                     match self.state {
                         ExerciseState::Warmup => {
                             self.state = ExerciseState::Exercise;
-                            self.remaining_s = self.exercise.exercise_s;
+                            self.remaining_s = self.setup.exercise_s;
                         }
                         ExerciseState::Exercise => {
                             self.remaining_sets -= 1;
@@ -225,12 +224,12 @@ impl Component for AppModel {
                                 sender.input_sender().send(AppInput::StartStop).unwrap();
                             } else {
                                 self.state = ExerciseState::Rest;
-                                self.remaining_s = self.exercise.rest_s;
+                                self.remaining_s = self.setup.rest_s;
                             }
                         }
                         ExerciseState::Rest => {
                             self.state = ExerciseState::Exercise;
-                            self.remaining_s = self.exercise.exercise_s;
+                            self.remaining_s = self.setup.exercise_s;
                         }
                     }
                 }
@@ -242,7 +241,72 @@ impl Component for AppModel {
     }
 }
 
+struct AppModel {
+    exerciser: Controller<ExerciseModel>,
+}
+
+#[relm4::component]
+impl SimpleComponent for AppModel {
+    type Init = ();
+    type Input = ();
+    type Output = ();
+
+    view! {
+        adw::Window {
+            #[name = "leaflet"]
+            adw::Leaflet {
+                set_can_navigate_back: true,
+                append = &gtk::Box {
+                    set_width_request: 300,
+                    set_orientation: gtk::Orientation::Vertical,
+                    append: left_header = &adw::HeaderBar {
+                        set_title_widget: Some(&adw::WindowTitle::new("Test Title", "Test Subtitle")),
+                        // set_show_end_title_buttons: false,
+                    },
+                    gtk::Label {
+                        set_label: "Test 1",
+                    },
+                    gtk::Label {
+                        set_label: "Test 2"
+                    }
+                },
+                append = &gtk::Separator::new(gtk::Orientation::Vertical),
+                append = &gtk::Box {
+                    set_hexpand: true,
+                    set_orientation: gtk::Orientation::Vertical,
+                    adw::HeaderBar {
+                        set_title_widget: Some(&adw::WindowTitle::new("Main Title", "Main Subtitle")),
+                    },
+                    #[local_ref]
+                    exerciser -> adw::Clamp,
+                }
+            }
+        }
+    }
+
+    fn init(
+        _init: Self::Init,
+        root: &Self::Root,
+        sender: ComponentSender<Self>,
+    ) -> ComponentParts<Self> {
+        let model = AppModel {
+            exerciser: ExerciseModel::builder()
+                .launch(())
+                .forward(sender.input_sender(), identity),
+        };
+        let exerciser = model.exerciser.widget();
+        let widgets = view_output!();
+        widgets
+            .leaflet
+            .bind_property("folded", &widgets.left_header, "show_end_title_buttons")
+            .sync_create()
+            .build();
+        ComponentParts { model, widgets }
+    }
+}
+
 fn main() {
-    let app = RelmApp::new("relm4.test.simple_manual");
-    app.run::<AppModel>(10usize);
+    // gio::resources_register_include!("hiit.gresource").expect("Failed to register resources.");
+    let app = RelmApp::new("org.safeworlds.hiit");
+    app.run::<AppModel>(());
 }
