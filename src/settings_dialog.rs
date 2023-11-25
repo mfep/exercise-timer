9ccb40d4adaf6845103f0a4a056a05d7a25f6562
Lőrinc Serfőző <lorinc.serfozo@mailbox.org>
2023-11-25 18:55:07 +0100

    Using PreferencesWindow

--- src/settings_dialog.rs
+++ src/settings_dialog.rs
@@ -18,46 +18,30 @@ impl Component for SettingsDialogModel {
     type CommandOutput = ();
 
     view! {
-        adw::Window {
-            set_modal: true,
+        adw::PreferencesWindow {
             set_visible: true,
-            set_width_request: 400,
-            adw::ToolbarView {
-                add_top_bar = &adw::HeaderBar {
-                    #[wrap(Some)]
-                    set_title_widget = &adw::WindowTitle {
-                        set_title: "Preferences",
-                    },
-                },
-                #[wrap(Some)]
-                set_content = &gtk::ScrolledWindow {
-                    set_vexpand: true,
-                    adw::Clamp {
-                        gtk::Box {
-                            set_margin_all: 12,
-                            set_spacing: 8,
-                            set_hexpand: true,
-                            adw::PreferencesGroup {
-                                set_title: "Exercise defaults",
-                                gtk::ListBox {
-                                    add_css_class: "boxed-list",
-                                    adw::SpinRow {
-                                        set_title: "Warmup time",
-                                        set_subtitle: "seconds",
-                                        #[wrap(Some)]
-                                        #[name = "warmup_adjust"]
-                                        set_adjustment = &gtk::Adjustment {
-                                            set_lower: 0.0,
-                                            set_upper: 999.0,
-                                            set_step_increment: 1.0,
-                                            add_binding: (&init.warmup_s, "value"),
-                                        },
-                                    },
-                                }
-                            }
-                        }
+            set_default_height: 400,
+            set_default_width: 400,
+            set_search_enabled: false,
+            add = &adw::PreferencesPage {
+                add = &adw::PreferencesGroup {
+                    set_title: "Exercise defaults",
+                    gtk::ListBox {
+                        add_css_class: "boxed-list",
+                        adw::SpinRow {
+                            set_title: "Warmup time",
+                            set_subtitle: "seconds",
+                            #[wrap(Some)]
+                            #[name = "warmup_adjust"]
+                            set_adjustment = &gtk::Adjustment {
+                                set_lower: 0.0,
+                                set_upper: 999.0,
+                                set_step_increment: 1.0,
+                                add_binding: (&init.warmup_s, "value"),
+                            },
+                        },
                     }
-                },
+                }
             },
         }
     }
